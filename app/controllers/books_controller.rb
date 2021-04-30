f7f20af379c4b2949c076eb0b139418653731330
YutakaAihara <youguisufanyuan@gmail.com>
2021-04-30 06:12:52 +0000

    [Update]successメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    end
+      
   end
 
   #本の詳細画面
