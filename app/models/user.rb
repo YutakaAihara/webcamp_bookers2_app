8131aa34d5bf4d7d1a081c88a5c425a991936cb8
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-02 09:11:21 +0000

    [Add]フォロー機能関係の既存viewに加筆

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,24 +14,24 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
                                   dependent: :destroy
-  has_many :following, through: active_relationships, source: :followed
-  has_many :followers, through: passive_relationships, source: :follower
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attachment :profile_image
 
   validates :name, presence: true, uniqueness: true,
                    length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(other_user)
     following << other_user
   end
-  
+
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
-  
-  def following?
+
+  def following?(other_user)
     following.include?(other_user)
   end
 end
