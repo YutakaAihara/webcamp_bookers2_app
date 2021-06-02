5e11fb1ecb446759c218f9224f96ddf111ac39b3
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-02 08:12:15 +0000

    [Add]フォロワーの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
   has_many :following, through: active_relationships, source: :followed
+  has_many :followers, through: passive_relationships, source: :follower
 
   attachment :profile_image
 
@@ -29,4 +33,5 @@ class User < ApplicationRecord
   
   def following?
     following.include?(other_user)
+  end
 end
