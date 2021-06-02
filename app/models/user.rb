a72caab96e81c2a4cf36bacace0ae17866b051e2
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-02 08:00:38 +0000

    [Add]RelationshipとUserのアソシエーション・Relationshipのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,25 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many
-  
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: active_relationships, source: :followed
+
   attachment :profile_image
 
   validates :name, presence: true, uniqueness: true,
                    length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
+  
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?
+    following.include?(other_user)
 end
