36e0b7f27f84e5ab30db2a1836cbbe43b69ad3bd
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-08 05:46:55 +0000

    [Add]Groupモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_105817) do
+ActiveRecord::Schema.define(version: 2021_06_08_054523) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_06_04_105817) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "book_id"], name: "index_favorites_on_user_id_and_book_id", unique: true
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
