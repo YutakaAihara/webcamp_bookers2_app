99720c7d7f3102aea6e584e6b5f5c02b078d070d
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-10 13:35:10 +0000

    [Add]メール作成画面、メール確認画面、create,showコントローラ編集

--- app/controllers/group_messages_controller.rb
+++ app/controllers/group_messages_controller.rb
@@ -3,18 +3,23 @@ class GroupMessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @group_message = GroupMessage.new
   end
-  
+
   def create
+    @group = Group.find(params[:group_id])
+    @group_message = GroupMessage.new(group_message_params)
+    @group_message.group_id = @group.id
+    @group_message.save
+    redirect_to group_group_message_path(@group, @group_message)
   end
-  
+
   def show
-    @group_message.find(params[:id])
+    @group_message = GroupMessage.find(params[:id])
   end
-  
+
   private
-  
+
   def group_message_params
-    params.require(:group_message).permit(:name, :message)
+    params.require(:group_message).permit(:mail_title, :mail_body, :group_id)
   end
 
 end
